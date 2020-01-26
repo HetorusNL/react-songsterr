661d197aff82321522a5ed7983eda0ebe7dab82e
Hetorus NL <timkleinnijenhuis@hotmail.com>
2020-01-27 00:55:46 +0100

    set development port to 3001 for reverse proxy remote testing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<p>
+	  Dev version!
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
